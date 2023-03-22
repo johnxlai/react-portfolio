28a8df9a095f356e26e3139a2b361aa59fb97564
John Lai <lai.john.py@gmail.com>
2023-03-21 21:08:38 -0400

    Add nav componet

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import About from './pages/About';
 
 export default function Main() {
-  return <div>test</div>;
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+  };
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+
+      {renderPage()}
+    </div>
+  );
 }
