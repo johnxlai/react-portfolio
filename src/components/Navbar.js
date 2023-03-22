b736e4af93629461b0f4ace804e2ea0a902708c7
John Lai <lai.john.py@gmail.com>
2023-03-21 21:17:29 -0400

    Add about me in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,62 @@
 import React from 'react';
 
-function Navbar() {
+function Navbar({ currentPage, handlePageChange }) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          John Lai
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <ul>
+        <li>
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+    </div>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    //   <div className="container-fluid">
+    //     <a className="navbar-brand" href="/">
+    //       John Lai
+    //     </a>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target="#navbarNav"
+    //       aria-controls="navbarNav"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation">
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarNav">
+    //       <ul className="navbar-nav">
+    //         <li className="nav-item">
+    //           <a className="nav-link active" aria-current="page" href="/">
+    //             About
+    //           </a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="/">
+    //             Portfolio
+    //           </a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="/">
+    //             Contact
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 }
+
+export default Navbar;
---
+++ src/components/pages/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return <div className="aboutme">about me</div>;
+}
+
+export default About;
