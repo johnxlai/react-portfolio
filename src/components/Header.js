7b0d416be1e676dd10ba17ffc452831dc3f72bab
John Lai <lai.john.py@gmail.com>
2023-03-22 00:33:45 -0400

    header stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,36 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="bg-slate-900 text-white p-7">
-      <h1 className="text-primary">John Lai's Portfolio</h1>
+    // <div className="relative btn-test">
+    //   <h1 className="test">test</h1>
+    //   <button class="btn-gone">text1</button>
+    //   <button class="btn-gone">text2</button>
+    // </div>
+    <header className="main-header">
+      <section title="Navigation Container" class="container">
+        <div class="row">
+          <div class="col-12 col-header">
+            <div class="logo-container">
+              <img
+                class="jl-logo"
+                src="./assets/img/jlai-logo-2023-no-bg.svg"
+                alt="John Lai's Logo"
+              />
+              <h2 class="text-uppercase text-white">John Lai</h2>
+            </div>
+
+            <button
+              class="mobile-nav-toggle"
+              aria-controls="main-nav"
+              aria-expanded="false">
+              <span class="sr-only">Menu</span>
+              <i class="fa fa-bars"></i>
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+      </section>
     </header>
   );
 }
-
 export default Header;
