36bf10101d2dbb3f2d750236c7c5729e5bf0aa14
John Lai <lai.john.py@gmail.com>
2023-03-26 15:46:00 -0400

    Add input form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,15 @@ import React from 'react';
 export default function Form() {
   return (
     <div className="container mx-auto">
-      <form className="w-1/2 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        action=""
+        method="get"
+        id="contactForm"
+        className="w-1/2 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="name">
+            htmlFor="name">
             Name
           </label>
           <input
@@ -15,12 +19,13 @@ export default function Form() {
             id="name"
             type="text"
             placeholder="Your Name"
+            required
           />
         </div>
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="email">
+            htmlFor="email">
             Email
           </label>
           <input
@@ -29,29 +34,31 @@ export default function Form() {
             id="email"
             type="email"
             placeholder="Email"
+            required
           />
         </div>
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="message">
+            htmlFor="message">
             Message
           </label>
           <textarea
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             name="message"
             rows="5"
+            required
           />
         </div>
 
-        <p class="form-error text-red-400 mb-3">Error Message Here</p>
+        <p className="form-error text-red-400 mb-3">Error Message Here</p>
 
-        <div class="flex items-center justify-between">
-          <button
-            class="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button">
-            Submit
-          </button>
+        <div className="flex items-center justify-between">
+          <input
+            className="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+            value="Submit"
+          />
         </div>
       </form>
     </div>
