341ffeefaa2c1e0bea6b1615abfd7375816cec1b
John Lai <lai.john.py@gmail.com>
2023-03-26 18:22:45 -0400

    Update contact form size

--- src/components/Form.js
+++ src/components/Form.js
@@ -44,7 +44,7 @@ export default function Form() {
         action=""
         method="get"
         id="contactForm"
-        className="w-1/2 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        className="md:w-1/2 mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
